b18d7b13058c8123f018664d2234f507d22e35dd
adogbeji <baka_1993@yahoo.co.uk>
2025-01-22 19:23:41 +0000

    Install dotenv package & create .env file

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 // const http = require('http');
 const path = require('path');
 
+require('dotenv').config({path: './.env'});
 const express = require('express');
 const bodyParser = require('body-parser');
 
@@ -15,7 +16,7 @@ app.use('/', (req, res, next) => {
     res.render('home');
 });
 
-const PORT = 3000;
+const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}...`);
