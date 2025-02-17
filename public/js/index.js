0c163811b405d9cf343ae7d13cc4fcae235cb09c
adogbeji <baka_1993@yahoo.co.uk>
2025-02-17 20:29:14 +0000

    Remove old JavaScript & add new code for home page navbar

--- public/js/index.js
+++ public/js/index.js
@@ -1,26 +1,42 @@
 'use strict';
 
 
-/* When the user clicks on the button,
-toggle between hiding and showing the dropdown content */
-function openTopnavClickDropDownTwo() {
+// SECOND RESPONSIVE DROPDOWN
+
+/* When the user clicks on the dots, toggle between hiding and showing the dropdown content */
+function openTopnavLinkDotDropDownTwo() {
     // Original Code
-    // if (document.getElementById('topNavDropdown_2').classList.contains('topnav__dropdown-content-2--show')) {
-    //     document.getElementById('topNavDropdown_2').classList.remove('topnav__dropdown-content-2--show');
+    // if (document.getElementById('topNavLinkDotDropdown_2').classList.contains('topnav__link-dot_2-dropdown-content--show')) {
+    //     document.getElementById('topNavLinkDotDropdown_2').classList.remove('topnav__link-dot_2-dropdown-content--show')
     // } else {
-    //     document.getElementById('topNavDropdown_2').classList.add('topnav__dropdown-content-2--show');
+    //     document.getElementById('topNavLinkDotDropdown_2').classList.add('topnav__link-dot_2-dropdown-content--show')
     // }
-  
+
     // Shortened Code
-    document.getElementById('topNavDropdown_2').classList.toggle('topnav__dropdown-content-2--show');
+    document.getElementById('topNavLinkDotDropdown_2').classList.toggle('topnav__link-dot_2-dropdown-content--show');
 }
 
-document.addEventListener('click', function(e) {
-    if (e.target !== document.querySelector('.topnav__dropdown-btn-2')) {
-        const dropdown = document.querySelector('.topnav__dropdown-content-2');
+// document.addEventListener('click', function(e) {
+//     if (e.target !== document.querySelector('.topnav__link-dot-icon-box-2')) {
+//         const dropdown = document.querySelector('.topnav__link-dot_2-dropdown-content');
 
-        if (dropdown.classList.contains('topnav__dropdown-content-2--show')) {
-            dropdown.classList.remove('topnav__dropdown-content-2--show');
-        }
+//         if (dropdown.classList.contains('topnav__link-dot_2-dropdown-content--show')) {
+//             dropdown.classList.remove('topnav__link-dot_2-dropdown-content--show');
+//         }
+//     }
+// });
+
+
+
+// HAMBURGER MENU ICON
+
+/* Toggle between showing and hiding the navigation menu links when the user clicks 
+   on the hamburger menu/bar icon */
+   function openMobileMenu() {
+    let x = document.getElementById('mobileLinks');
+    if (x.style.display === 'block') {
+        x.style.display = 'none'; 
+    } else {
+        x.style.display = 'block';
     }
-});
+}
